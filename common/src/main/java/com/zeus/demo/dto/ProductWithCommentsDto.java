8a2db10578ec5a165b80d1e8cf5416bae486a0dc
formation <hassinkhlifi20@gmail.com>
2025-06-26 13:25:41 +0100

    test

---
+++ common/src/main/java/com/zeus/demo/dto/ProductWithCommentsDto.java
@@ -0,0 +1,13 @@
+package com.zeus.demo.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class ProductWithCommentsDto {
+    private ProductDto product;
+    private List<CommentDto> comment;
+}
