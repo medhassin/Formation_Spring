b846908d6ddba301caf6e7a98972859722321b77
medhassin <hassinkhlifi20@gmail.com>
2025-06-27 13:30:01 +0100

    Config doker

--- oracle-19c/init-user.sql
+++
@@ -1,5 +0,0 @@
--- init-user.sql
--- Creates user and grants privileges on first run
-ALTER SESSION SET "_ORACLE_SCRIPT"=true;
-CREATE USER zeus IDENTIFIED BY pwdtrainer;
-GRANT CONNECT, RESOURCE, DBA TO zeus;
